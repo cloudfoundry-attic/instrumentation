afe0701b6ab95f4d41c8fd90436014557339763d
Gregg Van Hove and Mark Rushakoff <pair+gvanhove+mrushakoff@pivotallabs.com>
2013-08-30 12:20:16 -0700

    add a second endpoint that does not use query params

--- apps/sinatra-instrumentation/app.rb
+++ apps/sinatra-instrumentation/app.rb
@@ -13,9 +13,19 @@ get '/' do
   until Time.now > later
   end
 
-  if params.has_key?('short_response')
-    buffer.size.to_s
-  else
-    buffer
+  buffer
+end
+
+get '/busy/:busy/size/:response_size' do |busy, response_size|
+  busy = busy.to_f
+  response_size = response_size.to_i
+
+  # allocate memory
+  buffer = '0' * response_size
+
+  later = (Time.now + busy)
+  until Time.now > later
   end
+
+  buffer
 end
